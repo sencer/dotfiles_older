8fa4945605941c8f4cbbf9866c267d89c0275fe4
Sencer Selcuk <sencerselcuk@gmail.com>
2013-05-01 11:27:33 -0400

    vmd improvements

--- .vmd/state.tcl
+++ .vmd/state.tcl
@@ -3,7 +3,7 @@ proc state {} {
   global y
   global z
   if { [file exists view.vmd] } {
-    source view.vmd
+    catch { source view.vmd }
   } else {
     foreach mid [molinfo list] {
       molinfo $mid set {
@@ -37,6 +37,7 @@ user add key W {
   set fildes [open view.vmd w]
 
   foreach mol [molinfo list] {
+    puts $fildes "catch {"
     puts $fildes "mol delrep 0 top"
     if [info exists representations($mol)] {
       set i 0
@@ -46,52 +47,18 @@ user add key W {
         puts $fildes "mol color $c"
         puts $fildes "mol selection {$s}"
         puts $fildes "mol material $m"
-        puts $fildes "mol addrep top"
+        puts $fildes "mol addrep $mol"
         if {[string length $pbc]} {
           puts $fildes "mol showperiodic top $i $pbc"
           puts $fildes "mol numperiodic top $i $numpbc"
     }
-    puts $fildes "mol selupdate $i top $selupd"
-    puts $fildes "mol colupdate $i top $colupd"
-    puts $fildes "mol scaleminmax top $i $colminmax"
-    puts $fildes "mol smoothrep top $i $smooth"
-    puts $fildes "mol drawframes top $i {$framespec}"
-    set cpnum 0
-    foreach cp $cplist {
-      foreach { center color normal status } $cp { break }
-      puts $fildes "mol clipplane center $cpnum $i top {$center}"
-      puts $fildes "mol clipplane color  $cpnum $i top {$color }"
-      puts $fildes "mol clipplane normal $cpnum $i top {$normal}"
-      puts $fildes "mol clipplane status $cpnum $i top {$status}"
-      incr cpnum
-    }
-    if { !$on } {
-      puts $fildes "mol showrep top $i 0"
-    }
     incr i
   } 
 }
-if {[molinfo $mol get drawn] == 0} {
-  puts $fildes "molinfo top set drawn 0"
-}
-if {[molinfo $mol get active] == 0} {
-  puts $fildes "molinfo top set active 0"
-}
-puts $fildes "set viewpoints(\[molinfo top\]) [list $viewpoints($mol)]"
-puts $fildes "lappend viewplist \[molinfo top\]"
-if {$mol == [molinfo top]} {
-  puts $fildes "set topmol \[molinfo top\]"
-}
+puts $fildes "  molinfo $mol set {center_matrix rotate_matrix scale_matrix global_matrix} [list $viewpoints($mol)]"
+puts $fildes "}"
 puts $fildes "\# done with molecule $mol"
 } 
-puts $fildes "foreach v \$viewplist \{"
-  puts $fildes "  molinfo \$v set {center_matrix rotate_matrix scale_matrix global_matrix} \$viewpoints(\$v)"
-  puts $fildes "\}"
-  puts $fildes "unset viewplist"
-  if {[llength [molinfo list]] > 0} {
-    puts $fildes "mol top \$topmol"
-    puts $fildes "unset topmol"
-}
 save_colors $fildes
 # save_labels $fildes
 puts $fildes "set x \$x"
@@ -99,6 +66,7 @@ puts $fildes "set y \$y"
 puts $fildes "set z \$z"
 puts $fildes "pbc box -shiftcenter \"$x $y $z\""
 puts $fildes "pbc wrap -shiftcenter \"$x $y $z\""
+puts $fildes "pbc box -off"
 close $fildes
 }
 }
